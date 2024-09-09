3a1f60acbb8ba3be4f1a8b870349f2b62955eceb
Save Sabanal <save.sabanal@gmail.com>
2024-09-09 10:27:20 -0500

    updates

--- Device_Test/Device_Test.cpp
+++ Device_Test/Device_Test.cpp
@@ -1,35 +1,45 @@
-#include "MyCustomClass.h"
+#include "Device_Test.h"
 #include "allocateMem.h"
 #include "commandmessenger.h"
+#include <FastLED.h>
+
+#define LED_PIN 4
+#define NUM_LEDS 20 // 150 LEDs in the full strip
+
+CRGB leds[NUM_LEDS];
 
 /* **********************************************************************************
     This is just the basic code to set up your custom device.
     Change/add your code as needed.
 ********************************************************************************** */
 
-MyCustomClass::MyCustomClass(uint8_t Pin1, uint8_t Pin2)
+Device_Test::Device_Test(uint8_t Pin1, uint8_t Pin2)
 {
     _pin1 = Pin1;
     _pin2 = Pin2;
 }
 
-void MyCustomClass::begin()
+void Device_Test::begin()
 {
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(20);
+  leds[LEDPosition] = CRGB::White;
+  FastLED.show();
 }
 
-void MyCustomClass::attach(uint16_t Pin3, char *init)
+void Device_Test::attach(uint16_t Pin3, char *init)
 {
     _pin3 = Pin3;
 }
 
-void MyCustomClass::detach()
+void Device_Test::detach()
 {
     if (!_initialised)
         return;
     _initialised = false;
 }
 
-void MyCustomClass::set(int16_t messageID, char *setPoint)
+void Device_Test::set(int16_t messageID, char *setPoint)
 {
     /* **********************************************************************************
         Each messageID has it's own value
@@ -41,8 +51,8 @@ void MyCustomClass::set(int16_t messageID, char *setPoint)
         Put in your code to enter this mode (e.g. clear a display)
 
     ********************************************************************************** */
-    int32_t  data = atoi(setPoint);
-    uint16_t output;
+    // int32_t  data = atoi(setPoint);
+    // uint16_t output;
 
     // do something according your messageID
     switch (messageID) {
@@ -51,8 +61,9 @@ void MyCustomClass::set(int16_t messageID, char *setPoint)
     case -2:
         // tbd., get's called when PowerSavingMode is entered
     case 0:
-        output = (uint16_t)data;
-        data   = output;
+        // output = (uint16_t)data;
+        // data   = output;
+        setLEDPosition(atof(setPoint));
         break;
     case 1:
         /* code */
@@ -65,7 +76,26 @@ void MyCustomClass::set(int16_t messageID, char *setPoint)
     }
 }
 
-void MyCustomClass::update()
+void Device_Test::update()
 {
     // Do something which is required regulary
+    if(prevLEDPosition != LEDPosition)
+    {
+        prevLEDPosition = LEDPosition;
+        FastLED.clear();
+        FastLED.show();
+        leds[LEDPosition] = CRGB::White;
+        FastLED.show();
+
+    }
+}
+
+void Device_Test::setLEDPosition(float value)
+{
+    LEDPosition = scaleValue(value, -1.0, 1.0, 0, 19);
+}
+
+float Device_Test::scaleValue(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
--- Device_Test/Device_Test.h
+++ Device_Test/Device_Test.h
@@ -2,10 +2,10 @@
 
 #include "Arduino.h"
 
-class MyCustomClass
+class Device_Test
 {
 public:
-    MyCustomClass(uint8_t Pin1, uint8_t Pin2);
+    Device_Test(uint8_t Pin1, uint8_t Pin2);
     void begin();
     void attach(uint16_t Pin3, char *init);
     void detach();
@@ -15,4 +15,10 @@ public:
 private:
     bool    _initialised;
     uint8_t _pin1, _pin2, _pin3;
+
+    uint16_t LEDPosition = 10;
+    uint16_t prevLEDPosition = 10;
+
+    float scaleValue(float x, float in_min, float in_max, float out_min, float out_max);
+    void setLEDPosition(float value);
 };
